018635d294b01e934ad1749f6f2e2451f247595d
maks alekseev <maks-alekseev94@mail.ru>
2023-02-14 14:20:26 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 15648;
+        int bonus = 20;
+        int miles = ticket / bonus;
+        System.out.println(miles);
+    }
+}
